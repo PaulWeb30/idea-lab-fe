09cb0bb78ffa0b524c8c0da70fa4d4fe2fff7cbd
Dmytro-Korol <koroldmytrouitm@gmail.com>
2026-04-12 14:34:49 +0200

    Created ideas page and nav bar for simple route navigation

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { QueryProvider } from '@/providers/query-provider'
+import Link from 'next/link'
 import './globals.css'
 
 export const metadata: Metadata = {
@@ -11,6 +12,12 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body>
+        <div className="flex justify-end gap-6 bg-gray-200 p-4">
+          <Link className='hover:text-gray-600' href={"/ideas"}>Ideas</Link>
+          <Link className='hover:text-gray-600' href={"/signup"}>Signup</Link>
+          <Link className='hover:text-gray-600' href={"/login"}>Login</Link>
+          <Link className='hover:text-gray-600' href={"/"}>Home</Link>
+        </div>
         <QueryProvider>{children}</QueryProvider>
       </body>
     </html>
